40618fa6bb6e1c7da7b8922f17ad01dcf7824f71
MichaelNyembe <michael@psybergate.co.za>
2022-02-18 20:10:52 +0200

    Updates

---
+++ precache-manifest.5b8a87f6ea22f72da48097dd52ffe627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb2e8b9f24e3e860b4ac0d2c2e841e9",
+    "url": "/covid-stats/index.html"
+  },
+  {
+    "revision": "3a0a89b27a385c6afbf0",
+    "url": "/covid-stats/static/css/main.b13ad566.chunk.css"
+  },
+  {
+    "revision": "4645eb866c305ac33d02",
+    "url": "/covid-stats/static/js/2.ff5ec9d2.chunk.js"
+  },
+  {
+    "revision": "9ac7d1fb223f2f9a1e9e",
+    "url": "/covid-stats/static/js/3.79a04dbb.chunk.js"
+  },
+  {
+    "revision": "3a0a89b27a385c6afbf0",
+    "url": "/covid-stats/static/js/main.5d5015a3.chunk.js"
+  },
+  {
+    "revision": "6128c170156b204b88ad",
+    "url": "/covid-stats/static/js/runtime~main.6ddec779.js"
+  }
+]);
